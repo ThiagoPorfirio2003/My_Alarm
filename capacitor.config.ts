886302836e29c526961e5364c1297b30d0c984b2
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-05-01 21:01:07 -0300

    Icon y splashScreen animado: Agregado

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'MyAlarm',
+  appId: 'io.ionic.MyAlarm',
+  appName: 'Mi alarma',
   webDir: 'www'
 };
 
