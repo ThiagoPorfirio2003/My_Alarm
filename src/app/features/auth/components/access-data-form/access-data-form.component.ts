25df8c4cbf822c3ed16df64aee4af056295f2f1e
Thiago Lisandro Porfirio <thiagolisandroporfirio@gmail.com>
2024-05-29 17:51:34 -0300

    Cuando se desloguea no aparecen los mensajes de error de los inputs

--- src/app/features/auth/components/access-data-form/access-data-form.component.ts
+++ src/app/features/auth/components/access-data-form/access-data-form.component.ts
@@ -32,7 +32,7 @@ export class AccessDataFormComponent implements OnChanges
     this.showHidePassword();
 
     this.emailErrorText = 'Correo: ';
-    this.passwordErrorText = 'Clave: '
+    this.passwordErrorText = 'Clave: ';
 
     this.loginForm = inject(FormBuilder).group({
       email: ['', [Validators.required, Validators.email, Validators.maxLength(60)]],
@@ -47,8 +47,8 @@ export class AccessDataFormComponent implements OnChanges
   ngOnChanges() : void 
   {
     this.loginForm.setValue(this.fastUser);
-    this.changeEmailErrorText();
-    this.changePasswordErrorText();
+    this.emailErrorText = 'Correo: ';
+    this.passwordErrorText = 'Clave: ';
   }
 
   public showHidePassword()
